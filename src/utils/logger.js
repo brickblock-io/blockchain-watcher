afdf26ffbb605c30cd56613df5127c01538a1c2f
mattgstevens <mattgstevens@gmail.com>
2018-04-04 11:18:01 +0200

    chore: optional environment variable for log level

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,5 +2,9 @@
 
 const bunyan = require('bunyan')
 
+// making this env var optional to show that the preferred log level is 'info'
+const logLevel = process.env.LOG_LEVEL || 'info'
+
 // here we config logger for all workers
-module.exports = (name: string) => bunyan.createLogger({ name })
+module.exports = (name: string) =>
+  bunyan.createLogger({ name, level: logLevel })
