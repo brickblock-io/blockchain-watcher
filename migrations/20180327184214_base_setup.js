eb04b43a6ef95b96c2efe0c8dc7dd11fdc741c3b
mattgstevens <mattgstevens@gmail.com>
2018-04-04 11:10:55 +0200

    fix: all entries in migrations base_setup should be not-nullable column types

--- migrations/20180327184214_base_setup.js
+++ migrations/20180327184214_base_setup.js
@@ -4,24 +4,25 @@ const up = knex =>
   knex.schema
     .createTable('contact_data', table => {
       table.text('email').primary()
-
-      table.text('ethereum_address').index()
-      table.text('name')
+      ;[table.text('ethereum_address').index(), table.text('name')].forEach(x =>
+        x.notNullable()
+      )
     })
     .createTable('email_receipts', table => {
-      table.increments('id').primary()
-
-      table.text('contact_email').index()
-      table.text('event_name').index()
-      table.text('transaction_hash').index()
+      table.text('transaction_hash').primary()
+      ;[
+        table.text('contact_email').index(),
+        table.text('event_name').index()
+      ].forEach(x => x.notNullable())
     })
     .createTable('events', table => {
       table.increments('id').primary()
-
-      table.text('contract_address').index()
-      table.json('event_data')
-      table.text('event_name').index()
-      table.text('transaction_hash').index()
+      ;[
+        table.text('contract_address').index(),
+        table.json('event_data'),
+        table.text('event_name').index(),
+        table.text('transaction_hash').index()
+      ].forEach(x => x.notNullable())
     })
 
 const down = knex =>
