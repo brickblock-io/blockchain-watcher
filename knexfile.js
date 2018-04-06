494fb79391572e0f4d65684fa0836a1bf72e0f7f
mattgstevens <mattgstevens@gmail.com>
2018-04-06 14:14:04 +0200

    chore: add POSTGRES_HOST to knexfile to allow for local docker runs

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ const config = {
     connection: {
       database: process.env.POSTGRES_DATABASE,
       user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
       password: process.env.POSTGRES_PASSWORD
     },
     migrations: {
